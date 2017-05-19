f4fe556b6595d771eb590fd2fce73c640a01adec
Balajee Mohan <bala.mnups@gmail.com>
2017-05-19 15:13:04 +0530

    [Bala] Adding gp2Caching Layer

---
+++ gp2-caching.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo 'this file exists solely to interact with gpg-agent, it and the .asc file can be deleted' > mystique-gpg-cacher.txt
+gpg2 -ab mystique-gpg-cacher.txt
+#rm -f mystique-gpg-cacher.txt mystique-gpg-cacher.txt.asc
